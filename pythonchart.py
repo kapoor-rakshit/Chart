8ae27643313a843364897a9d0cae7897a9d4ef1f
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-06-08 17:55:06 +0530

    Add files via upload

--- pythonchart.py
+++ pythonchart.py
@@ -9,7 +9,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Go to <br> /linechart : for linechart <br> /barchart : for barchart <br> /piechart : for piechart"
+	return "Go to <br> /linechart : for linechart <br> /barchart : for barchart <br> /piechart : for piechart <br> /radarchart : for radarchart"
 
 @app.route('/linechart')
 def line():
@@ -20,16 +20,42 @@ def line():
 
 @app.route('/barchart')
 def bar():
+	legend="Data"
 	labels=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"]
 	values=[10,9,12,6,2,5,7,6]
-	return render_template("bar.html",values=values,labels=labels)
+	return render_template("bar.html",values=values,labels=labels,legend=legend)
 
 @app.route('/piechart')
 def pie():
 	labels=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"]
 	values=[10,9,12,6,2,5,7,6]
 	colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC" , "#FAFAAA"]
-	return render_template("pie.html",st=zip(values,labels,colors))
+	return render_template("pieanddoughnut.html",st=zip(values,labels,colors))
+
+@app.route('/radarchart')
+def radar():
+	labels=["DCS","DSD","AMMC","EEIM","DSP"]
+	value1=[68,69,85,81,89]
+	value2=[85,88,65,95,96]
+	return render_template("radar.html",value1=value1,value2=value2,labels=labels)
+
+@app.route('/polarchart')
+def polar():
+	labels=["DCS","DSD","AMMC","EEIM","DSP"]
+	values=[60,60,81,84,82]
+	return render_template("polar.html",labels=labels,values=values)
+
+@app.route('/bubblechart')
+def bubble():
+	return render_template("bubble.html")
+
+@app.route('/scatterchart')
+def scatter():
+	return render_template("scatter.html")
+
+@app.route('/mixedchart')
+def mixed():
+	return render_template("mixed.html")
 
 if __name__=="__main__":
 	app.debug=True
